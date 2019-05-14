c7381f9d983ff67fb04ba750b7a04cbfd967acd9
zxfjd3g <258147149@qq.com>
2019-05-14 15:42:48 +0800

    form

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -21,11 +21,47 @@ class Login extends Component {
     // 阻止事件的默认行为
     event.preventDefault()
 
+    // 对所有表单字段进行检验
+    this.props.form.validateFields((err, values) => {
+      // 检验成功
+      if (!err) {
+        console.log('提交登陆的ajax请求', values)
+      } else {
+        console.log('检验失败!')
+      }
+    });
+
     // 得到form对象
-    const form = this.props.form
-    // 获取表单项的输入数据
-    const values = form.getFieldsValue()
-    console.log('handleSubmit()', values)
+    // const form = this.props.form
+    // // 获取表单项的输入数据
+    // const values = form.getFieldsValue()
+    // console.log('handleSubmit()', values)
+  }
+
+  /*
+  对密码进行自定义验证
+  */
+  /*
+   用户名/密码的的合法性要求
+     1). 必须输入
+     2). 必须大于等于4位
+     3). 必须小于等于12位
+     4). 必须是英文、数字或下划线组成
+    */
+  validatePwd = (rule, value, callback) => {
+    console.log('validatePwd()', rule, value)
+    if(!value) {
+      callback('密码必须输入')
+    } else if (value.length<4) {
+      callback('密码长度不能小于4位')
+    } else if (value.length>12) {
+      callback('密码长度不能大于12位')
+    } else if (!/^[a-zA-Z0-9_]+$/.test(value)) {
+      callback('密码必须是英文、数字或下划线组成')
+    } else {
+      callback() // 验证通过
+    }
+    // callback('xxxx') // 验证失败, 并指定提示的文本
   }
 
   render () {
@@ -45,7 +81,24 @@ class Login extends Component {
           <Form onSubmit={this.handleSubmit} className="login-form">
             <Item>
               {
-                getFieldDecorator('username', {})(
+                /*
+              用户名/密码的的合法性要求
+                1). 必须输入
+                2). 必须大于等于4位
+                3). 必须小于等于12位
+                4). 必须是英文、数字或下划线组成
+               */
+              }
+              {
+                getFieldDecorator('username', { // 配置对象: 属性名是特定的一些名称
+                  // 声明式验证: 直接使用别人定义好的验证规则进行验证
+                  rules: [
+                    { required: true, whitespace: true, message: '用户名必须输入' },
+                    { min: 4, message: '用户名至少4位' },
+                    { max: 12, message: '用户名最多12位' },
+                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须是英文、数字或下划线组成' },
+                  ],
+                })(
                   <Input
                     prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                     placeholder="用户名"
@@ -55,7 +108,13 @@ class Login extends Component {
             </Item>
             <Form.Item>
               {
-                getFieldDecorator('password', {})(
+                getFieldDecorator('password', {
+                  rules: [
+                    {
+                      validator: this.validatePwd
+                    }
+                  ]
+                })(
                   <Input
                     prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                     type="password"
@@ -79,9 +138,22 @@ class Login extends Component {
 
 /*
 1. 高阶函数
+    1). 一类特别的函数
+        a. 接受函数类型的参数
+        b. 返回值是函数
+    2). 常见
+        a. 定时器: setTimeout()/setInterval()
+        b. Promise: Promise(() => {}) then(value => {}, reason => {})
+        c. 数组遍历相关的方法: forEach()/filter()/map()/reduce()/find()/findIndex()
+        d. 函数对象的bind()
+        e. Form.create()() / getFieldDecorator()()
+    3). 高阶函数更新动态, 更加具有扩展性
 
 2. 高阶组件
-
+    1). 本质就是一个函数
+    2). 接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
+    3). 作用: 扩展组件的功能
+    4). 高阶组件也是高阶函数: 接收一个组件函数, 返回是一个新的组件函数
  */
 /*
 包装Form组件生成一个新的组件: Form(Login)
