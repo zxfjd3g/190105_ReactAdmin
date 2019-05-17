ca7b570c4d241038f1da3a8d4637af7dc6a462f2
zxfjd3g <258147149@qq.com>
2019-05-17 15:56:10 +0800

    category

--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -3,19 +3,140 @@ import {
   Card,
   Table,
   Button,
-  Icon
+  Icon,
+  message
 } from 'antd'
 
 import LinkButton from '../../components/link-button'
+import {reqCategorys} from '../../api'
 
 /*
 商品分类路由
  */
 export default class Category extends Component {
+
+  state = {
+    loading: false, // 是否正在获取数据中
+    categorys: [], // 一级分类列表
+    subCategorys: [], // 二级分类列表
+    parentId: '0', // 当前需要显示的分类列表的父分类ID
+    parentName: '', // 当前需要显示的分类列表的父分类名称
+  }
+
+  /*
+  初始化Table所有列的数组
+   */
+  initColumns = () => {
+    this.columns = [
+      {
+        title: '分类的名称',
+        dataIndex: 'name', // 显示数据对应的属性名
+      },
+      {
+        title: '操作',
+        width: 300,
+        render: (category) => ( // 返回需要显示的界面标签
+          <span>
+            <LinkButton>修改分类</LinkButton>
+            {/*如何向事件回调函数传递参数: 先定义一个匿名函数, 在函数调用处理的函数并传入数据*/}
+            {this.state.parentId==='0' ? <LinkButton onClick={() => this.showSubCategorys(category)}>查看子分类</LinkButton> : null}
+
+          </span>
+        )
+      }
+    ]
+  }
+
+
+  /*
+  异步获取一级/二级分类列表显示
+   */
+  getCategorys = async () => {
+
+    // 在发请求前, 显示loading
+    this.setState({loading: true})
+    const {parentId} = this.state
+    // 发异步ajax请求, 获取数据
+    const result = await reqCategorys(parentId)
+    // 在请求完成后, 隐藏loading
+    this.setState({loading: false})
+
+    if(result.status===0) {
+      // 取出分类数组(可能是一级也可能二级的)
+      const categorys = result.data
+      if(parentId==='0') {
+        // 更新一级分类状态
+        this.setState({
+          categorys
+        })
+      } else {
+        // 更新二级分类状态
+        this.setState({
+          subCategorys: categorys
+        })
+      }
+    } else {
+      message.error('获取分类列表失败')
+    }
+  }
+
+  /*
+  显示指定一级分类对象的二子列表
+   */
+  showSubCategorys = (category) => {
+    // 更新状态
+    this.setState({
+      parentId: category._id,
+      parentName: category.name
+    }, () => { // 在状态更新且重新render()后执行
+      console.log('parentId', this.state.parentId) // '0'
+      // 获取二级分类列表显示
+      this.getCategorys()
+    })
+    // setState()不能立即获取最新的状态: 因为setState()是异步更新状态的
+    // console.log('parentId', this.state.parentId) // '0'
+  }
+
+  /*
+  显示指定一级分类列表
+   */
+  showCategorys = () => {
+    // 更新为显示一列表的状态
+    this.setState({
+      parentId: '0',
+      parentName: '',
+      subCategorys: []
+    })
+  }
+
+  /*
+  为第一次render()准备数据
+   */
+  componentWillMount () {
+    this.initColumns()
+  }
+
+  /*
+  执行异步任务: 发异步ajax请求
+   */
+  componentDidMount () {
+    // 获取一级分类列表显示
+    this.getCategorys()
+  }
+
   render() {
 
+    // 读取状态数据
+    const {categorys, subCategorys, parentId, parentName, loading} = this.state
+
     // card的左侧
-    const title = '一级分类列表'
+    const title = parentId === '0' ? '一级分类列表' : (
+      <span>
+        <LinkButton onClick={this.showCategorys}>一级分类列表</LinkButton>
+        <Icon type='arrow-right' style={{marginRight: 5}}/>
+        <span>{parentName}</span>
+      </span>
+    )
     // Card的右侧
     const extra = (
       <Button type='primary'>
@@ -24,69 +145,16 @@ export default class Category extends Component {
       </Button>
     )
 
-    const dataSource = [
-      {
-        "parentId": "0",
-        "_id": "5ca9d695b49ef916541160ba",
-        "name": "家用电器",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5ca9d6c0b49ef916541160bb",
-        "name": "电脑",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5ca9d6c9b49ef916541160bc",
-        "name": "图书",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5ca9d6e9b49ef916541160bd",
-        "name": "服装",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5ca9d70cb49ef916541160be",
-        "name": "食品",
-        "__v": 0
-      },
-      {
-        "parentId": "0",
-        "_id": "5ca9d727b49ef916541160bf",
-        "name": "玩具",
-        "__v": 0
-      }
-    ]
-
-    const columns = [
-      {
-        title: '分类的名称',
-        dataIndex: 'name', // 显示数据对应的属性名
-      },
-      {
-        title: '操作',
-        width: 300,
-        render: () => ( // 返回需要显示的界面标签
-          <span>
-            <LinkButton>修改分类</LinkButton>
-            <LinkButton>查看子分类</LinkButton>
-          </span>
-        )
-      }
-    ];
-
     return (
       <Card title={title} extra={extra}>
         <Table
           bordered
           rowKey='_id'
-          dataSource={dataSource}
-          columns={columns}/>
+          loading={loading}
+          dataSource={parentId==='0' ? categorys : subCategorys}
+          columns={this.columns}
+          pagination={{defaultPageSize: 5, showQuickJumper: true}}
+        />
       </Card>
     )
   }
