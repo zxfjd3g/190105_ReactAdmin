8e42b04873743a92b4e0bd060df1638c9d20e924
zxfjd3g <258147149@qq.com>
2019-06-01 23:26:33 +0800

    hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {HashRouter, Route, Switch} from 'react-router-dom'
 
 import Login from './pages/login/login'
 import Admin from './pages/admin/admin'
@@ -12,12 +12,12 @@ export default class App extends Component {
 
   render () {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch> {/*只匹配其中一个*/}
           <Route path='/login' component={Login}></Route>
           <Route path='/' component={Admin}></Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 }
